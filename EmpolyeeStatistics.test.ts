549862dbca37bdd8ef9b987e87bb8e439fa6fe81
kormonditamas2222 <t.kormond2006@gmail.com>
2026-04-12 16:30:33 +0200

    new tests

--- EmpolyeeStatistics.test.ts
+++ EmpolyeeStatistics.test.ts
@@ -3,6 +3,9 @@ import { type Employee,  EmployeeStatistics} from "./EmployeeStatistics.js";
 
 let list: Employee[];
 let list2: Employee[];
+let lista: Employee[];
+let stat1: EmployeeStatistics;
+let stat2: EmployeeStatistics;
 
 beforeEach(() => {
     list = [
@@ -15,10 +18,28 @@ beforeEach(() => {
         {name: "Barna Bernát", age: 42, salary: 1500000},
         {name: "Celldömölki Cecil", age: 35, salary: 800000}
     ]
+    lista = [];
+    stat1 = new EmployeeStatistics(list);
+    stat2 = new EmployeeStatistics(list2);
 });
 
 describe("EmployeeStatistics()", () => {
     test("osztály létrehozása listában elemekkel", () => {
         expect(() => new EmployeeStatistics(list)).not.toThrow();
     })
+    test("osztály létrehozása üres listával", () => {
+        expect(() => new EmployeeStatistics(lista)).toThrow();
+    })
+    test("osztály létrehozása nem létező listával", () => {
+        expect(() => new EmployeeStatistics(badList)).toThrow();
+    })
+})
+describe("getMaxSalary()", () => {
+    test("függvény helyes végeredmény", () => {
+        expect(stat1.getMaxSalary()).toBe(900000);
+    })
+    test("helyes eredmény másik adathalmazzal", () => {
+        expect(stat2.getMaxSalary()).toBe(1500000);
+    })
+    
 })
